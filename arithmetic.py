d179a9740121717b0d182bd18d6a0980b06a91c2
Sakshi <92329377+SakshiMhasitkar@users.noreply.github.com>
2021-10-11 23:53:04 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+a = int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+add=a+b
+diff=abs(a-b)
+multiply = a*b
+divide = (a//b)
+remainder = a%b
+print(f"The sum of {a} and {b} is:",add)
+print(f"The absolute difference of {a} and {b} is:",diff)
+print(f"the product of {a} and {b} is:",multiply)
+print(f"the remainder when {b} is divided by {a} is:",remainder)
